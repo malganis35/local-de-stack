d041a6ee44746ace1f585431669f396ff9b84554
caotrido <cao-tri.do@keyrus.com>
2024-12-29 17:37:36 +0100

    [MODIFIED] change local variable to global variable in .env file

--- minio_starter.py
+++ minio_starter.py
@@ -1,16 +1,20 @@
 from minio import Minio
 import io
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Minio connection details
-MINIO_ENDPOINT = 'your-minio-endpoint'
-MINIO_ACCESS_KEY = 'your-minio-access-key'
-MINIO_SECRET_KEY = 'your-minio-secret-key'
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT') # 'your-minio-endpoint'
+MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY') # 'your-minio-access-key'
+MINIO_SECRET_KEY = os.getenv('MINIO_SECRET_KEY') # 'your-minio-secret-key'
 
 # Connect to Minio
 minio = Minio(MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, secure=False)
 
 # Create a bucket
-bucket_name = 'your-bucket-name'
+bucket_name = "bucket-keyrusds-test" # 'your-bucket-name'
 if not minio.bucket_exists(bucket_name):
     minio.make_bucket(bucket_name)
 
